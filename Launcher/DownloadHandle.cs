cffdf23885da45e2d1171d8bb6509de3688272b6
ryakr <nopes2@yahoo.com>
2016-09-01 20:23:26 -0700

    More File Download Work

--- Launcher/DownloadHandle.cs
+++ Launcher/DownloadHandle.cs
@@ -8,25 +8,30 @@ using System.Windows.Forms;
 using System.Net;
 using System.ComponentModel;
 using System.IO;
-using ICSharpCode.SharpZipLib.Tar;
 
 namespace Launcher
 {
     class DownloadHandle
     {
         public static ProgressBar DownloadBar;
-
+        private static string lastDownloadedFile = "RYANFAPSTOFURRYPORN.FAP";
+        private static List<string> downloadList = new List<string>();
         public static void Initialize(ProgressBar DownloadProgress)
         {
             DownloadBar = DownloadProgress;
             Ping pingSender = new Ping();
             PingReply reply = pingSender.Send("api.github.com");
 
-            if(reply.Status == IPStatus.Success)
+            if (reply.Status == IPStatus.Success)
             {
                 MessageBox.Show("Connected Successfully");
-            } else
-            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(CompletedFilelist);
+                webClient.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/ryakr/Launcher/master/filelist.txt"), "filelist.txt");
+
+
+            }
+            else {
                 MessageBox.Show("Failed To Connect");
             }
         }
@@ -35,9 +40,12 @@ namespace Launcher
             WebClient webClient = new WebClient();
             webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
             webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
-            string zipname = "Game" + ".tar.gz";
-            String ZipSave = "E:\\test\\" + zipname;
-            webClient.DownloadFileAsync(new Uri("https://github.com/ryakr/Launcher/tarball/master"), ZipSave);
+            if (downloadList.Count > 0)
+            {
+                lastDownloadedFile = downloadList[0];
+                string downloadlink = "https://raw.githubusercontent.com/ryakr/Launcher/master/" + lastDownloadedFile;
+                webClient.DownloadFileAsync(new Uri(downloadlink), lastDownloadedFile);
+            }
         }
         private static void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
         {
@@ -46,16 +54,21 @@ namespace Launcher
 
         private static void Completed(object sender, AsyncCompletedEventArgs e)
         {
-            MessageBox.Show("Download completed!");
-            String ZipSave = "E:\\test\\" + "Game.tar.gz";
-            Stream inStream = File.OpenRead(ZipSave);
 
-            TarArchive tarArchive = TarArchive.CreateInputTarArchive(inStream);
-            tarArchive.ExtractContents("E:\\test");
-            tarArchive.Close();
-
-            inStream.Close();
-
+            downloadList.Remove(lastDownloadedFile);
+            if (downloadList.Count > 0)
+            {
+                lastDownloadedFile = downloadList[0];
+                BeginDownload(sender, e);
+            }
+        }
+        private static void CompletedFilelist(object sender, AsyncCompletedEventArgs e)
+        {
+            foreach (string line in File.ReadAllLines("filelist.txt"))
+            {
+                downloadList.Add(line);
+            }
+            MessageBox.Show("List Indexed");
         }
     }
 }
